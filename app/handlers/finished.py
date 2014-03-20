8aa4c8928625be0851e9c47a7bcc5687e44a09c8
WAKAYAMA Shirou <shirou.faw@gmail.com>
2014-03-21 00:11:48 +0900

    add mail send function.

--- app/handlers/finished.py
+++ app/handlers/finished.py
@@ -12,6 +12,7 @@ import tempfile
 
 import boto
 from boto.ses.connection import SESConnection
+from jinja2 import Template
 
 
 class FinishedHandler(tornado.web.RequestHandler):
@@ -20,6 +21,32 @@ class FinishedHandler(tornado.web.RequestHandler):
 
     def initialize(self, conf):
         self.ses = self.connect_ses(conf)
+
+    def create_mail_body(self, email, url, lang="en", result="True"):
+        if result == "True":
+            fname = lang + ".success.j2"
+        else:
+            fname = lang + ".failed.j2"
+
+        t = os.path.join(os.path.dirname(__file__), "..", "mailtmpl", fname)
+
+        with open(t) as f:
+            t_str = f.read()
+        template = Template(t_str)
+
+        return template.render(url=url)
+
+    def send_email(self, conn, fromaddr, toaddr, body):
+        log = ltsvlogger.LTSVLoggerAdapter(logging.getLogger())
+        m = {"from": fromaddr, "to": toaddr}
+        log.info("sending email", **m)
+
+        subject = "menes result"
+        conn.send_email(fromaddr,
+                        subject,
+                        body,
+                        [toaddr],
+                        [fromaddr])
 
     def connect_ses(self, conf):
         conn = boto.ses.connect_to_region(conf['aws']['ses_region'])
@@ -32,6 +59,7 @@ class FinishedHandler(tornado.web.RequestHandler):
 
         email = self.get_argument("email", None)
         token = self.get_argument("token", None)
+        lang = self.get_argument("lang", "en")
         result = self.get_argument("result", None)
 
         m = {"email": email, "token": token, "result": result}
@@ -44,6 +72,8 @@ class FinishedHandler(tornado.web.RequestHandler):
         if self.auth(email, token) is False:
             self.send_error(403)
             return
+
+        conn = self.connect_ses(conf)
 
         if result == "True":
             suffix = ".pdf"
@@ -58,6 +88,16 @@ class FinishedHandler(tornado.web.RequestHandler):
 
         m = {"path": path}
         log.info("save to tempfile", **m)
+
+        url = os.path.join(conf['worker']['menes_url'], "download", token + suffix)
+
+        body = self.create_mail_body(email, url, lang, result)
+
+        fromaddr = conf['app']['from_addr']
+        self.send_email(conn, fromaddr, email, body)
+
+        m = {"email": email}
+        log.info("sendmail done", **m)
 
         result_encoded = json.dumps((True, "success"))
         self.write(result_encoded)
