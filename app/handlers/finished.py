d6d21280d0d237a2d3080f5bc9bc5dfdd96205fa
WAKAYAMA Shirou <shirou.faw@gmail.com>
2014-03-27 05:37:15 -0400

    when build failed, read from build log file and paste to mail. not attach.

--- app/handlers/finished.py
+++ app/handlers/finished.py
@@ -14,6 +14,8 @@ import boto
 from boto.ses.connection import SESConnection
 from jinja2 import Template
 
+MAX_BUILD_LOG_SIZE = 1024
+
 
 class FinishedHandler(tornado.web.RequestHandler):
     def auth(self, email, token):
@@ -22,11 +24,15 @@ class FinishedHandler(tornado.web.RequestHandler):
     def initialize(self, conf):
         self.ses = self.connect_ses(conf)
 
-    def create_mail_body(self, email, url, language="en", result="True"):
+    def create_mail_body(self, email, url, path, language="en", result="True"):
+        log = ltsvlogger.LTSVLoggerAdapter(logging.getLogger())
+        build_log = ""
         if result == "True":
             fname = language + ".success.j2"
         else:
             fname = language + ".failed.j2"
+            with open(path) as f:
+                build_log = f.read(MAX_BUILD_LOG_SIZE)
 
         t = os.path.join(os.path.dirname(__file__), "..", "mailtmpl", fname)
 
@@ -34,7 +40,7 @@ class FinishedHandler(tornado.web.RequestHandler):
             t_str = f.read()
         template = Template(t_str)
 
-        return template.render(url=url)
+        return template.render(url=url, build_log=build_log)
 
     def send_email(self, conn, fromaddr, toaddr, body):
         log = ltsvlogger.LTSVLoggerAdapter(logging.getLogger())
@@ -85,13 +91,14 @@ class FinishedHandler(tornado.web.RequestHandler):
         path = os.path.join(conf['app']['download_root'], token + suffix)
         fh = open(path,  'wb')
         fh.write(fileinfo['body'])
+        fh.close()
 
         m = {"path": path}
         log.info("save to tempfile", **m)
 
         url = os.path.join(conf['worker']['menes_url'], "download", token + suffix)
 
-        body = self.create_mail_body(email, url, language, result)
+        body = self.create_mail_body(email, url, path, language, result)
 
         fromaddr = conf['app']['from_addr']
         self.send_email(conn, fromaddr, email, body)
